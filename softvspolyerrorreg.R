0cafc2480d3c0c8ec042a48f79d374663ae9e812
Sablabra <57529128+Sablabra@users.noreply.github.com>
2021-08-15 19:06:46 +0200

    Add files via upload

--- softvspolyerrorreg.R
+++ softvspolyerrorreg.R
@@ -58,7 +58,7 @@ func <- function(a){
   return(out)
 }
 
-order = 15
+order = 14
 Price_error = matrix(0,order,1)
 Delta_error = matrix(0,order,1)
 for (h in 1:order){
@@ -81,6 +81,87 @@ for (h in 1:order){
   Delta_error[h] = round(mean(abs(delta-TrueDelta)),8)
 }
 
+##Price-only Softplus
+set.seed(1)
+T = 1
+K = 1
+vol = 0.2
+
+c = 0.01
+S = seq(0.10,1.99,c)
+
+CallPrice = TruePrice = ZeroRateBachelierCall(S,1,1,0.2)$Price
+CallDelta = TrueDelta = ZeroRateBachelierCall(S,1,1,0.2)$Delta
+
+dummy = sd(CallPrice)/sd(CallDelta)
+w = 1
+
+Criterion<-function(a) {
+  Dummy = a[1]
+  dif_dummy = 0
+  for (i in 1:((length(a)-1)/3)){
+    Dummy<-Dummy+a[3*i-1]*SoftPlus(a[3*i]*S + a[3*i+1])
+    dif_dummy = dif_dummy + a[3*i-1]*dXSoftPlus(a[3*i]*S+a[3*i+1])*a[3*i]
+  }
+  
+  return(w*sum((CallPrice-Dummy)^2)+(1-w)*sum((CallDelta-dif_dummy)^2))
+}
+
+Gradient <- function(a){
+  Dummy = a[1]
+  dif_dummy = 0
+  for (j in 1:((length(a)-1)/3)){
+    Dummy = Dummy+a[3*j-2]*SoftPlus(a[3*j-1]*S + a[3*j])
+    dif_dummy = dif_dummy+a[3*j-2]*dXSoftPlus(a[3*j-1]*S + a[3*j])*a[3*j-1]
+    Dummy = Dummy-CallPrice
+    dif_dummy = dif_dummy-CallDelta
+    Grad = matrix(0,length(a),1)
+    Grad[1] = w*2*sum(Dummy)
+  }
+  for (j in 1:((length(a)-1)/3)){
+    d1 = SoftPlus(a[3*j-1]*S + a[3*j])
+    dif_d1 = dXSoftPlus(a[3*j-1]*S+a[3*j])*a[3*j-1]
+    Grad[3*j-1] = w*2*sum(Dummy*d1)+(1-w)*2*sum(dif_dummy*dif_d1)
+    
+    d2 = a[3*j-2]*dXSoftPlus(a[3*j-1]*S + a[3*j])*S
+    dif_d2 = a[3*j-2]*dXSoftPlus2(a[3*j-1]*S+a[3*j])*S*a[3*j-1]+a[3*j-2]*dXSoftPlus(a[3*j-1]*S+a[3*j])
+    Grad[3*j] = w*2*sum(Dummy*d2)+(1-w)*2*sum(dif_dummy*dif_d2)
+    
+    d3 = a[3*j-2]*dXSoftPlus(a[3*j-1]*S + a[3*j])
+    dif_d3 = a[3*j-2]*dXSoftPlus2(a[3*j-1]*S+a[3*j])*a[3*j-1]
+    Grad[3*j+1] = w*2*sum(Dummy*d3)+(1-w)*2*sum(dif_dummy*dif_d3)
+  }
+  return(Grad)
+}
+
+func <- function(a){
+  out <- Criterion(a)
+  attr(out, 'gradient') <- Gradient(a)
+  return(out)
+}
+
+order = 14
+Price_errorsoftnoreg = matrix(0,order,1)
+Delta_errorsoftnoreg = matrix(0,order,1)
+for (h in 1:order){
+  if (h==1){
+    x = c(0,0,0,0)
+  } else {
+    z = runif(1)
+    x <- c(Coef$par, z)
+  }
+  
+  Coef <- optim(x, func, method = "BFGS")
+  
+  y = Regression_SP(Coef$par,S)
+  delta = 0
+  for (j in 1:((length(Coef$par)-1)/3)){
+    delta = delta + Coef$par[3*j-1]*dXSoftPlus(Coef$par[3*j]*S+Coef$par[3*j+1])*Coef$par[3*j]
+  }
+  
+  Price_errorsoftnoreg[h] = round(mean(abs(y-TruePrice)),8)
+  Delta_errorsoftnoreg[h] = round(mean(abs(delta-TrueDelta)),8)
+}
 ##regression error with same setup
 T = 1
 K = 1
@@ -152,6 +233,7 @@ for(j in 1:PolyDeg){
   
 }
 
+##Softplus vs Polynomial price
 x = Powers[4:length(Powers)]
 k = Powers[2:length(Powers)]
 plot(x,Price_errorreg,type = "l", col = "red",ylim=c(0,0.004),ylab = "error",xlab="order of polynomial")
@@ -159,10 +241,23 @@ lines(k, Price_error, col = "blue")
 legend("topright" , legend=c("Polynomial regression", "Softplus regression"),
        col=c("red","blue" ),lty=1,  cex = 0.75)
 
-#Delta
+#Softplus vs Polynomial Delta
 x = Powers[4:length(Powers)]
 k = Powers[2:length(Powers)]
 plot(x,Delta_errorreg,type = "l", col = "red",ylim=c(0,0.04),ylab = "error",xlab="order of polynomial")
 lines(k, Delta_error, col = "blue")
 legend("topright" , legend=c("Polynomial regression", "Softplus regression"),
        col=c("red","blue" ),lty=1,  cex = 0.75)
+
+##Softplus vs Softplus
+x = 1:14
+plot(x,Price_error,type = "l", col = "red",ylim=c(0,0.0008),ylab = "error",xlab="order of SoftPlus")
+lines(x, Price_errorsoftnoreg, col = "blue")
+legend("topright" , legend=c("no diff regulization", "with diff regulization"),
+       col=c("blue","red" ),lty=1,  cex = 0.75)
+
+x = 1:14
+plot(x,Delta_error,type = "l", col = "red",ylim=c(0,0.008),ylab = "error",xlab="order of SoftPlus")
+lines(x, Delta_errorsoftnoreg, col = "blue")
+legend("topright" , legend=c("no diff regulization", "with diff regulization"),
+       col=c("blue","red" ),lty=1,  cex = 0.75)
