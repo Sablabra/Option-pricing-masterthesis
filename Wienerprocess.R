f110f2c8f6a634dce2e5f666722874af1165de0d
Sablabra <57529128+Sablabra@users.noreply.github.com>
2021-08-16 09:20:45 +0200

    Update Wienerprocess.R

--- Wienerprocess.R
+++ Wienerprocess.R
@@ -10,8 +10,10 @@ w[1,] <- x0
 for(j in 1:n)
 {for(i in 2:(m+1)){
   dw <- sqrt(dt)*rnorm(1,0,1)
-  w[i,j] <- x[i-1,j] + dx
+  w[i,j] <- w[i-1,j] + dw
 }
 }
 t <- seq(0, T, dt)
-matplot(t, x[,1], type="l", lty=1, col=c("blue4"), ylab = "W(t)")
+#jpeg("Fig56.jpg", units="in", width=5, height=5, res=300)
+matplot(t, w[,1], type="l", lty=1, col=c("blue4"), ylab = "W(t)")
+dev.off()
